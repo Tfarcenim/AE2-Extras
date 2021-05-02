a4402f34610190ef228d1514c25f728fb37c1147
Domaman202 <vip.domaman@mail.ru>
2021-05-02 15:32:33 +0700

    update

--- src/main/java/tfar/ae2extras/mixin/CraftingCubeModelMixin.java
+++ src/main/java/tfar/ae2extras/mixin/CraftingCubeModelMixin.java
@@ -45,17 +45,12 @@ public abstract class CraftingCubeModelMixin {
     @Inject(method = "bake(Lnet/minecraftforge/client/model/IModelConfiguration;Lnet/minecraft/client/renderer/model/ModelBakery;Ljava/util/function/Function;Lnet/minecraft/client/renderer/model/IModelTransform;Lnet/minecraft/client/renderer/model/ItemOverrideList;Lnet/minecraft/util/ResourceLocation;)Lnet/minecraft/client/renderer/model/IBakedModel;"
             , at = @At("HEAD"), cancellable = true,remap = false)
     private void bakeAe2Extras(IModelConfiguration owner, ModelBakery bakery, Function<RenderMaterial, TextureAtlasSprite> spriteGetter, IModelTransform modelTransform, ItemOverrideList overrides, ResourceLocation modelLocation, CallbackInfoReturnable<IBakedModel> cir) {
-        if (this.type == AE2Extras.STORAGE_256K || this.type == AE2Extras.STORAGE_1M | this.type == AE2Extras.STORAGE_4M || this.type == AE2Extras.STORAGE_16M) {
-            TextureAtlasSprite ringCorner = spriteGetter.apply(RING_CORNER);
-            TextureAtlasSprite ringSideHor = spriteGetter.apply(RING_SIDE_HOR);
-            TextureAtlasSprite ringSideVer = spriteGetter.apply(RING_SIDE_VER);
-            cir.setReturnValue(GrossAE2Hacks.getModel(this,ringCorner, ringSideHor,ringSideVer, spriteGetter,LIGHT_BASE,this.type));
-        }
+        if (this.type == AE2Extras.STORAGE_256K || this.type == AE2Extras.STORAGE_1M | this.type == AE2Extras.STORAGE_4M || this.type == AE2Extras.STORAGE_16M)
+            cir.setReturnValue(GrossAE2Hacks.getModel(this,spriteGetter.apply(RING_CORNER), spriteGetter.apply(RING_SIDE_HOR), spriteGetter.apply(RING_SIDE_VER), spriteGetter,LIGHT_BASE,this.type));
     }
     @Inject(method = "getAdditionalTextures()Ljava/util/stream/Stream;",at = @At("RETURN"),cancellable = true,remap = false)
     private void addTextures(CallbackInfoReturnable<Stream<RenderMaterial>> cir) {
-        Stream<RenderMaterial> materialStream = cir.getReturnValue();
-        List<RenderMaterial> materialList = materialStream.collect(Collectors.toList());
+        List<RenderMaterial> materialList = cir.getReturnValue().collect(Collectors.toList());
         materialList.addAll(GrossAE2Hacks.materials);
         cir.setReturnValue(materialList.stream());
     }
